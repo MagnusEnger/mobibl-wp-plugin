687a0a1e991ba28a512de3609a124e393a524377
Magnus Enger <magnus@enger.priv.no>
2011-09-13 16:48:35 +0200

    Make placeholder text configurable

--- mobibl.php
+++ mobibl.php
@@ -10,6 +10,22 @@ Author URI: http://libriotech.no/
 Author Email: magnus@enger.priv.no
 */
 
+/*  Copyright 2011 Magnus Enger Libriotech (email : magnus@enger.priv.no)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 /**
  * mobiblsearch_Widget Class
  */
@@ -24,11 +40,12 @@ class mobiblsearch_Widget extends WP_Widget {
 	function widget( $args, $instance ) {
 		extract( $args );
 		$title = apply_filters( 'widget_title', $instance['title'] );
+		$placeholder = empty( $instance['placeholder'] ) ? 'emne/tittel/forfatter' : $instance['placeholder'];
 		echo $before_widget;
 		if ( $title )
 			echo $before_title . $title . $after_title; ?>
 		<form method="get" action="/glitre/api/index.php" id="bib-search">
-    <input type="search" name="q" id="search" value="" placeholder="emne/tittel/forfatter" />
+    <input type="search" name="q" id="search" value="" placeholder="<?php echo($placeholder); ?>" />
 		<input type="hidden" name="library" value="demo" /> <!-- NBNBNBNB!!! -->
     <input type="hidden" name="sort_by" value="year" />
     <input type="hidden" name="sort_order" value="descending" />
@@ -41,21 +58,28 @@ class mobiblsearch_Widget extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['placeholder'] = strip_tags($new_instance['placeholder']);
 		return $instance;
 	}
 
 	/** @see WP_Widget::form */
 	function form( $instance ) {
 		if ( $instance ) {
-			$title = esc_attr( $instance[ 'title' ] );
+			$title       = esc_attr( $instance[ 'title' ] );
+			$placeholder = esc_attr( $instance[ 'placeholder' ] );
 		}
 		else {
 			$title = __( 'New title', 'text_domain' );
+			$placeholder = __( 'emne/tittel/forfatter', 'text_domain' );
 		}
 		?>
 		<p>
 		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id('placeholder'); ?>"><?php _e('Placeholder:'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('placeholder'); ?>" name="<?php echo $this->get_field_name('placeholder'); ?>" type="text" value="<?php echo $placeholder; ?>" />
 		</p>
 		<?php 
 	}
