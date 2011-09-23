779f4020a646494e949f5dbc1dc9323392f78a59
Magnus Enger <magnus@enger.priv.no>
2011-09-23 18:34:18 +0200

    Add a widget for displaying news/blogposts
    They have been removed from the theme.

--- mobibl.php
+++ mobibl.php
@@ -94,9 +94,66 @@ class mobiblsearch_Widget extends WP_Widget {
 		<?php 
 	}
 
-} // class Foo_Widget
+} 
 
-// register Foo_Widget widget
+// register 
 add_action( 'widgets_init', create_function( '', 'return register_widget("mobiblsearch_Widget");' ) );
+
+/**
+ * mobiblnews_Widget Class
+ */
+class mobiblnews_Widget extends WP_Widget {
+	/** constructor */
+	function mobiblnews_Widget() {
+	  $widget_ops = array('classname' => 'widget_mobiblnews', 'description' => __( 'Displays news/blog items.', 'mobiblnews') );
+		parent::WP_Widget( 'mobiblnews_widget', $name = 'moBibl News/Blog', $widget_ops );
+	}
+
+	/** @see WP_Widget::widget */
+	function widget( $args, $instance ) {
+		extract( $args );
+		# Get parameters
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title; ?>
+      <ul id="news-menu" data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+        <?php endwhile;endif ?>
+      </ul>
+		<?php echo $after_widget;
+	}
+
+	/** @see WP_Widget::update */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title']       = strip_tags($new_instance['title']);
+		return $instance;
+	}
+
+	/** @see WP_Widget::form */
+	function form( $instance ) {
+		if ( $instance ) {
+	 		//Defaults
+  		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Search the catalogue', 'placeholder' => 'emne/tittel/forfatter', 'library' => 'demo') );
+			$title       = esc_attr( $instance[ 'title' ] );
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<?php 
+	}
+
+} 
+
+// register widget
+add_action( 'widgets_init', create_function( '', 'return register_widget("mobiblnews_Widget");' ) );
+
 
 ?>
