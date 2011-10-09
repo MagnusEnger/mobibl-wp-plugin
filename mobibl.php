3d9ea04787db6f24e846bd8c2da7057c0ad0afcd
Magnus Enger <magnus@enger.priv.no>
2011-10-09 14:30:08 +0200

    Issue 6. Disable unrelevant default widgets

--- mobibl.php
+++ mobibl.php
@@ -26,6 +26,28 @@ Author Email: magnus@enger.priv.no
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+// -------------------------------------------------------------------
+// Remove some of the default widgets that are not needed in the 
+// context of moBibl
+add_action( 'widgets_init', 'remove_default_widgets', 0 );
+function remove_default_widgets() {
+  if (function_exists( 'unregister_widget' )) {
+	  unregister_widget('WP_Widget_Pages');
+	  unregister_widget('WP_Widget_Calendar');
+	  unregister_widget('WP_Widget_Archives');
+	  unregister_widget('WP_Widget_Links');
+	  unregister_widget('WP_Widget_Meta');
+	  unregister_widget('WP_Widget_Search');
+	  // unregister_widget('WP_Widget_Text');
+	  unregister_widget('WP_Widget_Categories');
+	  unregister_widget('WP_Widget_Recent_Posts');
+	  unregister_widget('WP_Widget_Recent_Comments');
+	  unregister_widget('WP_Widget_RSS');
+	  unregister_widget('WP_Widget_Tag_Cloud');
+	  unregister_widget('WP_Nav_Menu_Widget');
+  }
+}
+
 /**
  * mobiblsearch_Widget Class
  */
